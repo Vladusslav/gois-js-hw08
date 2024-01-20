71fcfa757bd1ea03526e0d26c2ba40ff9c74dd86
Vladusslav <vladfedunetx@gmail.com>
2024-01-20 14:46:15 +0200

    hw-8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const items = document.querySelectorAll('.item');
-console.log(`Number of categories: ${items.length}`);
-
-items.forEach(function callback(el) {
-    console.log(`Category: ${el.firstElementChild.textContent}`);
-    console.log(`Elements: ${el.lastElementChild.childElementCount}`);
-})
